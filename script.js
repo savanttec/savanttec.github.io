b165ca8f848b2d98929189661ce288f4ade873da
Khalid Siddiqui <khalidrafiqsiddiqui@gmail.com>
2024-05-06 13:13:43 -0400

    c

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+import Backpack from "./classes.js";
+
+const everydayPack = new Backpack(
+  "Everyday Backpack",
+  30,
+  "grey",
+  15,
+  26,
+  26,
+  false
+);
 
 function update() {
   let websiteMessage = "This is a test website.";
   document.getElementById("demo").innerHTML = `${websiteMessage}`;
+ 
 }
 
 
