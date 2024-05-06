3925d8a503e44938ac2defa885c8eb85cb7f1ebc
Khalid Siddiqui <khalidrafiqsiddiqui@gmail.com>
2024-05-06 15:01:13 -0400

    c

---
+++ backpack.js
@@ -0,0 +1,13 @@
+import Backpack from "./classes.js";
+
+const everydayPack = new Backpack(
+  "Everyday Backpack",
+  30,
+  "grey",
+  15,
+  26,
+  26,
+  false
+); 
+
+export default everydayPack;
